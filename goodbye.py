e84ae5f06f916888de61dd40fd4ba9f98bf5e395
bakang1398 <bakangsebetlela@gmail.com>
2021-08-21 09:57:08 +0200

    Added a function that says bye to a string name

---
+++ goodbye.py
@@ -0,0 +1,10 @@
+''' Goodbye
+Write a method goodbye(name) that takes in a string name and returns a string saying bye to that name. 
+See the example calls. '''
+
+def goodbye():
+    name = input('Please enter your name: ')
+    bye = 'Goodbye'
+    print(bye, name,'!')
+
+goodbye()
